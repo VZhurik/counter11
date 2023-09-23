78393a64d1c76e1c0f457cfd7c1c3d48a738c187
VZhurik <valerazhurda@icloud.com>
2023-09-23 13:18:57 +0300

    counter test 1

--- counter11/ViewController.swift
+++ counter11/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+var count = 0
+    
+    @IBOutlet weak var counter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func plus(_ sender: Any) {
+        count = count + 1
+        counter.text = "\(count)"
 
-
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        count = count - 1
+        if count < 0 {
+            count = 0
+        }
+        counter.text = "\(count)"
+    }
+    @IBAction func reset(_ sender: Any) {
+        count = 0
+        counter.text = "\(count)"
+    }
 }
 
